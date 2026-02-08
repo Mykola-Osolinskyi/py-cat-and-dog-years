1a572e75a3a555c6468130bc7c580b48903727a2
Your NameMykola <mykola.osolinskyi@gmail.com>
2026-02-08 19:13:27 +0200

    all tests passed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0,0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age_real_cases(
+    cat_age: int,
+    dog_age: int,
+    expected: list[int]
+):
+    assert get_human_age(cat_age, dog_age) == expected
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (10, "string"),
+    ]
+)
+def test_get_human_age_should_raise_type_error(
+    cat_age: int,
+    dog_age: int
+):
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (10, -10),
+        (106, 10),
+    ]
+)
+def test_get_human_age_should_raise_attribute_error(
+    cat_age: int,
+    dog_age: int
+):
+    with pytest.raises(AttributeError):
+        get_human_age(cat_age, dog_age)
